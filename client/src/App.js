82995bd849334d17421f49e4e992d5f4b4e7d145
Shawn J Plackiyil <SJPlackiyil@stu.naperville203.org>
2024-12-04 14:23:48 -0600

    updating React notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
+import {Button} from "@mui/material";
+
+function About() {
+  return (
+    <h2>Learn Employability Competencies</h2>
+  );
+}
+
+function Home() {
+  return (
+    <h2>Employability Competencies</h2>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-        </p>
-      
-      </header>
-    </div>
-  );
+  return (<>
+    <img src={logo} className="App-logo" alt="logo" />
+    <BrowserRouter>
+      <Link to="/"><Button variant="contained">Home</Button></Link>
+      <Link to="/about"><Button variant="contained">About</Button></Link>
+      <br />
+      <h1>Essential Employability Competencies</h1>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/about" Component={About} />
+      </Routes>
+    </BrowserRouter>
+  </>);
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use((req, res, next) => {
+    if(/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path))
+        next();
+    else
+    {
+        res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+        res.header("Expires", "-1");
+        res.header("Pragma", "no-cache");
+        res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    }
+});
+
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 const PORT = process.env.PORT || 8080;
