7117b598317b9575c66a717446be58aee42ddb40
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2024-12-09 14:20:50 -0600

    Competencies Select
    Completed competencies drop-down and post route for skill description. ALMOST DONE!

--- client/src/Components/SelectSkill.js
+++ client/src/Components/SelectSkill.js
@@ -1,20 +1,54 @@
 import React, {useEffect, useState} from 'react';
-import {Select, MenuItem, FormControl, InputLabel} from "@mui/material";
+import {Select, MenuItem, FormControl, InputLabel, Input} from "@mui/material";
 
 const SelectSkill = () => {
-    const [data, setData] = useState([]);
+  // When we call the setData function, the data variable is set to a new value
+  const [data, setData] = useState([]);
+  const [selectedValue, setSelectedValue] = useState(1);
 
-  // Fetch JSON from the server
-  const fetchData = async() => {
-    try {
-        // Create a promise for the completed response
-        const response = await fetch("http://localhost:8080/skills");
-        if (!response.ok) {
-            throw new Error("Server response was not ok.");
-        }
-        const result = await response.json();
-        setData(result);
-
+  useEffect(() => {
+    // Fetch JSON from the server
+    const fetchData = async() => {
+      try {
+          // Create a promise for the completed response
+          const response = await fetch("http://localhost:8080/skills");
+          if (!response.ok) {
+              throw new Error("Server response was not ok.");
+          }
+          const result = await response.json();
+          setData(result);
+      } catch (error) {
+        console.log("Error fetching the data", error);
+      }
     }
-  }  
+
+    fetchData();
+  }, []);
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  }
+
+  return (
+    <div>
+      <FormControl fullWidth>
+        <InputLabel id="select-label">Select a Competency</InputLabel>
+        <Select 
+          labelId='select-label'
+          value={selectedValue}
+          onChange={handleChange}
+        >
+        {
+          data.map((item, index) => (
+            <MenuItem key={index} value={item.id}>
+              {item.skill}
+            </MenuItem>
+          ))
+        }
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
+
+export default SelectSkill;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,20 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config({path: ".env"});
 
+// Middleware to handles CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 
+        'http://localhost:3002'
+    );
+    res.setHeader('Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    res.setHeader('Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
+    next();
+});
+
 var mysql = require("mysql2");
 
 var con = mysql.createConnection({
@@ -23,6 +37,19 @@ app.get("/skills", (req, res) => {
     con.query("SELECT id, skill FROM Competency ORDER BY skill;",
         function(err, result) {
             if (err) throw err;
+            res.json(result);
+        }
+    );
+});
+
+app.post("/skills", (req, res) => {
+    const id = req.body.id;
+    con.query('SELECT skill, description FROM Competency WHERE id = ? LIMIT 1', 
+        [id],
+        function (err, result) {
+            if (err) {
+                res.status(500).json({error: 'Database query error'});
+            }
             res.json(result);
         }
     );
